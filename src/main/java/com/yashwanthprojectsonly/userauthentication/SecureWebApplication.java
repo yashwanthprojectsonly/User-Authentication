917d197507cb295518e44df94de7a01caa27cfa7
yashwanthprojectsonly <yashwanthprojectsonly@gmail.com>
2024-01-31 15:41:15 +0530

    Initial files

---
+++ src/main/java/com/yashwanthprojectsonly/userauthentication/SecureWebApplication.java
@@ -0,0 +1,13 @@
+package com.yashwanthprojectsonly.userauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureWebApplication.class, args);
+	}
+
+}
